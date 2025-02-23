f35cc4720e976128a962a0380dadbb380d2f383c
Moniruzzaman Shawon <59255748+Moniruzzaman-Shawon@users.noreply.github.com>
2025-02-24 03:26:46 +0600

    Add files via upload

---
+++ Flpoyd-Warshall Algorithm/floyd_warshall.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,e;
+    cin >> n >> e;
+    int adj_mat [n][n];
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++){
+            if(i == j)
+                adj_mat[i][j] = 0;
+            else 
+                adj_mat[i][j] = LLONG_MAX;
+        }
+    while(e--){
+        int a,b,c;
+        cin >> a >> b >> c;
+        adj_mat[a][b] = c;
+    
+        // adj_mat[b][a] = c; // if undirected then
+    }
+    //complexity -> O(N) + O(N) + O(N) 
+    //TOTAL = O(N^3)
+    for(int k = 0; k<n; k++){   //O(N)
+        for(int i = 0; i < n; i++){         //O(N)
+            for(int j = 0; j < n; j++){             //O(N)
+                //cout << i << " "<< k << " " << j <<endl;  to check 
+                if(adj_mat[i][k] != LLONG_MAX && adj_mat[k][j ] != LLONG_MAX && adj_mat[i][k] + adj_mat[k][j] < adj_mat[i][j])
+                adj_mat[i][j] = adj_mat[i][k] + adj_mat[k][j];   
+            }
+        }
+    }
+
+    
+    /*--------Print code------------*/
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if(adj_mat[i][j] == LLONG_MAX)
+                cout << "INF ";
+            else
+            cout << adj_mat[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
